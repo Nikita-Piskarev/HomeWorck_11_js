5a22e1df534741afa21797cc1868a2b852e2590f
Nikita Piskarev <npiskarev23@gmail.com>
2022-09-19 15:41:22 +0300

    Fix if on the switch commit

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ returnLengNum(24, 43, 132, 45, "Ivan", 465, "Valick");
 
 // Создайте функцию продвинутого калькулятора. Он принимает знак, а  затем неограниченное количество чисел, над которыми проведет указанную операцию.
 const numbers = [3, 6];
-const calculator = (sing, ...num) => {
-  if (sing === "+") {
-    return num.reduce(
-      (previousValue, currentValue) => previousValue + currentValue
-    );
-  }
-  if (sing === "-") {
-    return num.reduce(
-      (previousValue, currentValue) => previousValue - currentValue
-    );
-  }
-  if (sing === "*") {
-    return num.reduce(
-      (previousValue, currentValue) => previousValue * currentValue
-    );
-  }
-  if (sing === "/") {
-    return num.reduce(
-      (previousValue, currentValue) => previousValue / currentValue
-    );
+const calculator = (aperator, ...num) => {
+  switch (aperator) {
+    case "+":
+      return num.reduce(
+        (previousValue, currentValue) => previousValue + currentValue
+      );
+    case "-":
+      return num.reduce(
+        (previousValue, currentValue) => previousValue - currentValue
+      );
+    case "*":
+      return num.reduce(
+        (previousValue, currentValue) => previousValue * currentValue
+      );
+    case "/":
+      return num.reduce(
+        (previousValue, currentValue) => previousValue / currentValue
+      );
+    default:
+      return 'Аператор не найден'
   }
 };
-console.log(calculator('+',...numbers));
+console.log(calculator("*", ...numbers));
 
 // Имея массив чисел, найдите в нем самое большое. Не используйте циклы или методы перебора массива.
 
-const arrNum = [2,4,5,76,98,3,46,65];
+const arrNum = [2, 4, 5, 76, 98, 3, 46, 65];
 const maxNum = (...arr) => console.log(Math.max(...arr));
 maxNum(...arrNum);
