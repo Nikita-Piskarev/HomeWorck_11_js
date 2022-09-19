a0847125963945e6ba744956ddf94c499a89a295
Nikita Piskarev <npiskarev23@gmail.com>
2022-09-19 15:30:47 +0300

    Add task 2,3 commit

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
+"use strict";
 // Создайте функцию, которая принимает неограниченное количество входящих данных, и возвращает количество строк, переданных ей в этом массиве. Решить с использованием стрелочных функций.
-const returnLengNum = (...nums) => nums.length;
+const returnLengNum = (...data) => console.log(data.length);
+
+returnLengNum(24, 43, 132, 45, "Ivan", 465, "Valick");
+
+// Создайте функцию продвинутого калькулятора. Он принимает знак, а  затем неограниченное количество чисел, над которыми проведет указанную операцию.
+const numbers = [3, 6];
+const calculator = (sing, ...num) => {
+  if (sing === "+") {
+    return num.reduce(
+      (previousValue, currentValue) => previousValue + currentValue
+    );
+  }
+  if (sing === "-") {
+    return num.reduce(
+      (previousValue, currentValue) => previousValue - currentValue
+    );
+  }
+  if (sing === "*") {
+    return num.reduce(
+      (previousValue, currentValue) => previousValue * currentValue
+    );
+  }
+  if (sing === "/") {
+    return num.reduce(
+      (previousValue, currentValue) => previousValue / currentValue
+    );
+  }
+};
+console.log(calculator('+',...numbers));
+
+// Имея массив чисел, найдите в нем самое большое. Не используйте циклы или методы перебора массива.
+
+const arrNum = [2,4,5,76,98,3,46,65];
+const maxNum = (...arr) => console.log(Math.max(...arr));
+maxNum(...arrNum);
